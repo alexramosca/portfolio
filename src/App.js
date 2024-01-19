eb807c7884087cab3ce27d1ec5fbea0c9394ecb7
Alex Ramos <alex@ramos.com>
2024-01-19 10:16:50 -0400

    fixed importing bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {About} from './components/About'
 import { ScreenProvider } from './contexts/ScreenSize';
 import { Projects } from './components/Projects';
 import { Footer } from './components/Footer';
-import {motion} from 'framer-motion'
+
 
 
 function App() {
