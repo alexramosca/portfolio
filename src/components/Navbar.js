ff694c9f95481c020473a17bfab5a78b5cb8e04d
Alex Ramos <alex@ramos.com>
2024-01-19 10:30:39 -0400

    fixed one more import bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import 'font-awesome/css/font-awesome.min.css';
+
 import { useScreenContext } from '../contexts/ScreenSize';
 import {useState, useEffect} from 'react'
 import {NavLink} from './NavLink'
