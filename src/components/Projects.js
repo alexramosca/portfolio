afc35218aa0fae20962771d7032952a0600065f4
Alex Ramos <jramosneto01@mynbcc.ca>
2024-02-12 13:36:21 -0400

    Updates projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,44 @@
 import {Project} from './Project';
 import { useScreenContext } from '../contexts/ScreenSize';
-import { useContext } from 'react';
 export const Projects = ()=>{
     const {isPortraitScreen} = useScreenContext();
     let projects = [
-        {img: "/icons/camera.png", title: "Photographer Portifolio" , link: "https://alexramosca.github.io/", 
-            desc : "Portifolio designed to photographers.It was my first project in college. Technologies: JavaScript, HTML, CSS. Current Status:  Being rebuilt to include more advanced features."},
-        {img: "/icons/car.png", title: "UTrip" , link: "https://utrip-front.vercel.app/",
-            desc : "Full Stack application. It allows users to share trips across the country. Technologies: React, Node, sequelize. Current Status: Front-end is Online, back-end is under development. Users can log in, view trips on a database, and create their own"},
-        {img: "/icons/task.png", title: "Task Management App" , link: "#",
-            desc : "Elevate your productivity with our task management app. Current Status: Under Development"},
-        {img: "/icons/wheather.png", title: "Weather App" , link: "#",
-            desc : "Stay ahead of the weather with our user-friendly app. Current Status: Under Development"}
+        { 
+            img: "/icons/car.png", 
+            title: "UTrip", 
+            link: "https://utrip-front.vercel.app/",
+            desc: "Full stack application that allows users to share trips across the country. Technologies used include React, Node.js, and Sequelize. The front-end is online, and back-end development is in progress. Users can log in, view trips in a database, and create their own." 
+        },
+        { 
+            img: "/icons/museum.png", 
+            title: "Museum Transcript System", 
+            link: "https://transcribe.frederictonregionmuseum.com/",
+            desc: "This is a real-world project aimed at improving the transcription system for the Fredericton Region Museum. Technologies include JavaScript, PHP, Ajax, HTML, and Bootstrap. Currently under development, addressing transcription system enhancements and optimizations." 
+        },
+        { 
+            img: "/icons/bank.png", 
+            title: "HooBank", 
+            link: "https://hoobankbusiness.vercel.app/",
+            desc: "Front-end project showcasing skills in creating beautiful and functional websites. HooBank is a fictional bank website built with React and Tailwind CSS. Currently under development, focusing on UI/UX enhancements and feature implementation." 
+        },
+        { 
+            img: "/icons/task.png", 
+            title: "Task Management App", 
+            link: "#",
+            desc: "Back-end application providing a REST API for task management. Built with Node.js, Sequelize, and optimized database design. Currently in the development phase, focusing on REST API implementation and database management features." 
+        }
+    ];
+    
 
-    ]
+    
     return(
         <div className="projectsPage" id='projects'>
             <h4><span className='highlight'>Projects</span></h4>
             <div className={isPortraitScreen?"projectsWrapper":"projectsWrapper-md"}>
-                <Project img={projects[1].img} title={projects[1].title} link={projects[1].link} desc={projects[1].desc} />
-                <Project img={projects[0].img} title={projects[0].title} link={projects[0].link} desc={projects[0].desc} />
-                <Project img={projects[2].img} title={projects[2].title} link={projects[2].link} desc={projects[2].desc} />
-                <Project img={projects[3].img} title={projects[3].title} link={projects[3].link} desc={projects[3].desc} />
+                
+                {projects.map((project, index)=>
+                    <Project key={index} img={project.img} title={project.title} link= {project.link} desc={project.desc}/>
+                )}
 
                 
             </div>
